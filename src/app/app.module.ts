a8b3117beff1b6406ac2bdb9800c1809bc4b9855
cbozbuga41@gmail.com <cbozbuga41@gmail.com>
2022-09-16 21:05:47 +0300

    componentadded

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
+import { BrandComponent } from './components/brand/brand.component';
+import { NaviComponent } from './components/navi/navi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarComponent
+    CarComponent,
+    BrandComponent,
+    NaviComponent
   ],
   imports: [
     BrowserModule,
